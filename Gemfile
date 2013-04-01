f39e888429ae1743e56c9b57ee0353b662a5f245
Dave Sullivan <davsu123@googlemail.com>
2013-04-01 21:28:01 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+group :development do
+  gem 'mysql2'
+  # gem 'activerecord-mysql2-adapter','0.0.3'
+  gem 'guard'
+  gem 'guard-livereload'
+  gem 'rspec'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
+
+group :assets do
+   gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails','2.0.2'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd98ede5d8629d5e33f59ea984593558cc10bf7221fc5f8b8a0bdf855ccceea9950bbd910c226d915bd56e11ddf4ab14ffa40beaa0698b87fa09b8ce2739f4473'
